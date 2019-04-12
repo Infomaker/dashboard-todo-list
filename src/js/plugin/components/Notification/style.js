a2fe547ca9ca1dc878a2928244a8ae6823e6ef75
Martin Almqvist <martin.almqvist@infomaker.se>
2019-04-12 16:18:36 +0200

    Fixed export

--- src/js/plugin/components/Notification/style.js
+++ src/js/plugin/components/Notification/style.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components'
 import { GUI } from "Dashboard";
 
-export const CustomWrapper = styled(GUI.Wrapper)`
+const CustomWrapper = styled(GUI.Wrapper)`
     padding-top: 20px;
 `
 
-export const ComponentButton = styled(GUI.Button)`
+const ComponentButton = styled(GUI.Button)`
     margin-right: 50px;
     margin-top:20px;
 `
+
+export {
+    CustomWrapper,
+    ComponentButton
+}
