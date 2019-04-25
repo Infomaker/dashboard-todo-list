d0d9d17ebc4380a43a460a93c95948fa0190fb49
Patric Tysk <patric@Patrics-MacBook-Pro.local>
2019-04-25 10:21:31 +0200

    Hook test

--- src/js/plugin/components/ListDone/ListDone.jsx
+++ src/js/plugin/components/ListDone/ListDone.jsx
@@ -1,74 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { GUI } from 'Dashboard';
 import { List } from '@components/List/style'
 import { Paragraph } from '@components/Paragraph/style'
 
 const Fragment = React.Fragment;
 
-class ListDone extends Component {
+const ListDone = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            showAll: false
-        }
-    }
+    // This component uses a React Hook!
+    const [showAll, setShowAll] = useState(false);
 
-    render() {
-        const { items, changeDoneItem, removeItem } = this.props;
-        const { showAll } = this.state;
+    const { items, changeDoneItem, removeItem } = props;
 
-        const doneItems = items
-            .map(item => {
-                return {
-                    id: item.id,
-                    content: (
-                        <Fragment>
-                            <Paragraph
-                                strikeThrough={true}
-                                text={item.text}
-                            />
-                            <GUI.Button
-                                text={"Undo"}
-                                size={"large"}
-                                onClick={() => changeDoneItem(item, false)}
-                            />
-                            <GUI.Button
-                                text={"Delete"}
-                                size={"large"}
-                                onClick={() => removeItem(item)}
-                            />
-                        </Fragment>
-                    )
-                };
-            });
+    const doneItems = items
+        .map(item => {
+            return {
+                id: item.id,
+                content: (
+                    <Fragment>
+                        <Paragraph
+                            strikeThrough={true}
+                            text={item.text}
+                        />
+                        <GUI.Button
+                            text={"Undo"}
+                            size={"large"}
+                            onClick={() => changeDoneItem(item, false)}
+                        />
+                        <GUI.Button
+                            text={"Delete"}
+                            size={"large"}
+                            onClick={() => removeItem(item)}
+                        />
+                    </Fragment>
+                )
+            };
+        });
 
-        return (
-            <Fragment>
-                {doneItems.length > 0 && (
-                    <GUI.Checkbox
-                        label={"Show done items"}
-                        checked={showAll}
-                        onChange={checked =>
-                            this.setState({
-                                showAll: checked
-                            })
-                        }
-                    />
-                )}
-                {showAll &&
-                    <List
-                        before={
-                            <GUI.Heading
-                                level={"2"}
-                                text={"Things you have done:"}
-                            />
-                        }
-                        items={doneItems}
-                    />
-                }
-            </Fragment>);
-    }
+    return (
+        <Fragment>
+            {doneItems.length > 0 && (
+                <GUI.Checkbox
+                    label={"Show done items"}
+                    checked={showAll}
+                    onChange={checked =>
+                        setShowAll(checked)
+                    }
+                // onChange={checked =>
+                //     this.setState({
+                //         showAll: checked
+                //     })
+                // }
+                />
+            )}
+            {showAll &&
+                <List
+                    before={
+                        <GUI.Heading
+                            level={"2"}
+                            text={"Things you have done:"}
+                        />
+                    }
+                    items={doneItems}
+                />
+            }
+        </Fragment>);
+
 }
 
 export default ListDone
